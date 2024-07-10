002942c818fa1ec870a8d822c9aacb62c5baa53a
apanga01 <pallyssam@gmail.com>
2024-07-09 23:46:47 -0400

    updated styles

--- src/Components/Experiences/Experiences.jsx
+++ src/Components/Experiences/Experiences.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Experiences.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
 import Services_Data from '../../assets/services_data'
 import arrow_icon from '../../assets/arrow_icon.svg'
 
