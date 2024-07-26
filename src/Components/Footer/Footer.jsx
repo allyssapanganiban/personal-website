4f6193912c32cce077932f0adb660cde7e4dddc7
apanga01 <pallyssam@gmail.com>
2024-07-25 22:45:54 -0400

    updated styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -19,13 +19,13 @@ const Footer = ({ theme, toggleTheme }) => {
         </div>
       </div>
       <li className="footer-icon-container">
-        <img src={theme === "light" ? dark_icon : light_icon} alt="Toggle Theme" className='icon' onClick={toggleTheme}  />
+        <img src={theme === "light" ? dark_icon : light_icon} alt="Toggle Theme" className='footer-icon' onClick={toggleTheme}  />
         <a href="mailto:apanga01@villanova.edu" target="_blank" rel="noopener noreferrer">
-          <img src={theme === "light" ? mail_icon2 : mail_icon} alt="Mail" className='icon' /></a>
+          <img src={theme === "light" ? mail_icon2 : mail_icon} alt="Mail" className='footer-icon' /></a>
         <a href="https://www.github.com/apanga01" target="_blank" rel="noopener noreferrer">
-          <img src={theme === "light" ? github_icon2 : github_icon} alt="GitHub" className='icon' /></a>
+          <img src={theme === "light" ? github_icon2 : github_icon} alt="GitHub" className='footer-icon' /></a>
         <a href="https://www.linkedin.com/in/allyssapanganiban" target="_blank" rel="noopener noreferrer">
-          <img src={theme === "light" ? linkedin_icon2 : linkedin_icon} alt="LinkedIn" className='icon' /></a>
+          <img src={theme === "light" ? linkedin_icon2 : linkedin_icon} alt="LinkedIn" className='footer-icon' /></a>
       </li>
     </div>
   )
