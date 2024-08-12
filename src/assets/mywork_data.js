18ddb668bd7f244d5a5c7c7d2ed5b6d91793231e
apanga01 <pallyssam@gmail.com>
2024-08-12 01:08:14 -0400

    Added Education Bot project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,3 +1,4 @@
+import educationbot from '../assets/educationbot.png'
 import grindbuddy from '../assets/grindbuddy.png'
 import metrosync from '../assets/metrosync.png'
 import spotifysong from '../assets/spotifysong.png'
@@ -5,14 +6,22 @@ import spotifysong from '../assets/spotifysong.png'
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Grind Buddy (WIP)",
-        w_img: grindbuddy,
-        w_desc: "A web app that connects and recommends study partners in your school.",
-        w_stack: "Javascript (Next.js)",
-        w_github: "https://github.com/jayco12/Grind-Buddy/tree/main",
+        w_name:"Education Bot",
+        w_img: educationbot,
+        w_desc: "An AI-driven customer support RAG bot that assists navigating university.",
+        w_stack: "Javascript (Next.js), MaterialUI, Firebase, OpenAI API, Langchain, Supabase Vectore Store",
+        w_github: "https://github.com/allyssapanganiban/customer-support-ai",
     },
     {
         w_no:2,
+        w_name:"Grind Buddy (WIP)",
+        w_img: grindbuddy,
+        w_desc: "A web app that connects and recommends study partners in your school.",
+        w_stack: "Javascript (Next.js), Gemini API",
+        w_github: "https://github.com/jayco12/Grind-Buddy/tree/main",
+    },
+    {
+        w_no:3,
         w_name:"Spotify Song Recommendation",
         w_img: spotifysong,
         w_desc: "A class project to create a music recommendation system with unsupervised ML.",
@@ -20,7 +29,7 @@ const mywork_data = [
         w_github: "https://github.com/allyssapanganiban/spotify-song-recommendation",
     },
     {
-        w_no:3,
+        w_no:4,
         w_name:"MetroSync (WIP)",
         w_img: metrosync,
         w_desc: "A web app that shows live train updates for the NYC MTA transit lines.",
