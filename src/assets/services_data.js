0618a3736dcfd2fb5cf9f45c271c232d814a41e0
Allyssa <166740534+allyssapanganiban@users.noreply.github.com>
2025-07-07 23:23:13 -0400

    Updated projects, experience, and resume

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,12 +1,13 @@
 import girlswhocode from './girlswhocode.png'
 import headstarter from './headstarter.png'
 import villanovalogo from './villanovalogo.png'
+import extern_logo from './extern_logo.png'
 
 
 
 const Services_Data = [
     {
-        s_no:"Sept. 2023 - Present",
+        s_no:"Sept. 2023 - May 2025",
         s_pic: girlswhocode,
         s_name:"Vice-President",
         s_desc:"Co-lead an interdisciplinary mentorship program of 15+ members and spearhead workshops and coding sessions for women in tech.",
@@ -14,7 +15,7 @@ const Services_Data = [
         s_link: "https://girlswhocode.com/"
     },
     {
-        s_no:"July 2024 - Present",
+        s_no:"July 2024 - Sept. 2024",
         s_pic: headstarter,
         s_name:"Software Engineer Fellow",
         s_desc:"Build 5 AI projects and one final project with the goal to ship to 1000 users and/or generate $1000 in revenue in 7 weeks.",
@@ -27,6 +28,14 @@ const Services_Data = [
         s_name:"Teaching Assistant",
         s_desc:"Teach students in CSC 1700: Analysis of Algorithms. Assist professor by hosting office hours and attending labs for in-person assistance.",
         s_group: "Villanova University",
+        s_link: "https://www1.villanova.edu/university/liberal-arts-sciences/programs/computing-sciences.html"
+    },
+    {
+        s_no:"July 2025 - Present",
+        s_pic: extern_logo,
+        s_name:"Outamation - AI-Powered Workflow Automation Externship",
+        s_desc:"Built AI-powered pipelines and a document retrieval system for processing 200+ page mortgage documents using OCR, PDF parsing, and Retrieval-Augmented Generation techniques, delivering technical evaluations and a UI demo.",
+        s_group: "Extern",
         s_link: "https://www1.villanova.edu/university/liberal-arts-sciences/programs/computing-sciences.html"
     },
 ]
