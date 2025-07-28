8e8020de594ccad20b308b56a2cb6f0e1bbaef59
Allyssa <166740534+allyssapanganiban@users.noreply.github.com>
2025-07-28 11:18:10 -0400

    Updated work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,6 +3,7 @@ import grindbuddy from '../assets/grindbuddy.png'
 import metrosync from '../assets/metrosync.png'
 import spotifysong from '../assets/spotifysong.png'
 import marketcampaign from '../assets/marketcampaign.png'
+import readyrefrigerator from '../assets/readyrefrigerator.png'
 
 const mywork_data = [
     {
@@ -13,14 +14,6 @@ const mywork_data = [
         w_stack: "Javascript (Next.js), MaterialUI, Firebase, OpenAI API, Langchain, Supabase Vectore Store",
         w_github: "https://github.com/allyssapanganiban/customer-support-ai",
     },
-    // {
-    //     w_no:2,
-    //     w_name:"Grind Buddy (WIP)",
-    //     w_img: grindbuddy,
-    //     w_desc: "A web app that connects and recommends study partners in your school.",
-    //     w_stack: "Javascript (Next.js), Gemini API",
-    //     w_github: "https://github.com/jayco12/Grind-Buddy/tree/main",
-    // },
     {
         w_no:2,
         w_name:"Vanguard Marketing Campaign",
@@ -28,10 +21,27 @@ const mywork_data = [
         w_desc: "A semester-long campaign for Vanguard, combining market research, audience analysis, and UI mockups to align with their brand identity.",
         w_stack: "Canva, Figma, Powerpoint, Qualtrics",
         w_figma: "https://www.figma.com/design/dFvHZEmbzGXzbG787EwLWu/-CE--Marketing-Material?node-id=136-256&t=OLWa9huD3Xw7Y4cY-1",
-        w_powerpoint: "https://drive.google.com/file/d/1smnhnbtowfs7QcpKX0xiUDHVBTYXVUe9/view?usp=sharing"
+        w_powerpoint: "https://drive.google.com/file/d/1smnhnbtowfs7QcpKX0xiUDHVBTYXVUe9/view?usp=sharing",
+    },
+        {
+        w_no:3,
+        w_name:"Ready Refrigerator",
+        w_img: readyrefrigerator,
+        w_desc: "An AI-powered app that tracks ingredients and suggests personalized recipes to save time in the kitchen.",
+        w_stack: "Figma, React.js, OpenAI, Spoonacular API, Firebase",
+        w_github: "https://github.com/CSC4700-Spring2025-Org/ready-refrigerator",
+        w_powerpoint: "https://drive.google.com/file/d/1uiaCdfa_WHuAmeeouZQxmmmN0ki8uF55/view?usp=sharing",
+    },
+        {
+        w_no:4,
+        w_name:"Grind Buddy",
+        w_img: grindbuddy,
+        w_desc: "A web app that connects and recommends study partners in your school.",
+        w_stack: "Javascript (Next.js), Gemini API",
+        w_github: "https://github.com/jayco12/Grind-Buddy/tree/main",
     },
     {
-        w_no:3,
+        w_no:5,
         w_name:"Spotify Song Recommendation",
         w_img: spotifysong,
         w_desc: "A class project to create a music recommendation system with unsupervised ML.",
